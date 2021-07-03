efa6339a40e04297e64e700b7e55005c26c0f96b
william.ward@mines.sdsmt.edu <william.ward@mines.sdsmt.edu>
2021-07-03 11:37:33 -0600

    added save list class

---
+++ src/main/java/Sudoku/SaveList.java
@@ -0,0 +1,46 @@
+package Sudoku;
+
+import java.util.ArrayList;
+
+public class SaveList {
+    static final int BOARD_SIZE = 9;
+    ArrayList<ArrayList<Integer>> thisList = new ArrayList<ArrayList<Integer>>();
+    SaveList(){
+        initializeListSize();
+    }
+
+    private void initializeListSize(){
+        for(int i = 0; i < BOARD_SIZE * BOARD_SIZE; i = i + 1){
+            thisList.add(new ArrayList<Integer>());
+        }
+    }
+
+    public ArrayList<Integer> getList(int row, int col){
+        int index = getIndexFromRowCol(row, col);
+        if(index != -1){
+            return thisList.get(getIndexFromRowCol(row, col)); 
+        }
+        return null;
+    }
+
+    public boolean addList(int row, int col, ArrayList<Integer> insertList){
+        int index = getIndexFromRowCol(row, col);
+        if(index != -1){
+            thisList.set(index, insertList);
+            return true;
+        }
+        return false;
+    }
+
+    public int getIndexFromRowCol(int row, int col){
+        int index = row*BOARD_SIZE+col;
+        if(index >= 0 && index <= 80){
+            return index;
+        }
+        return -1;
+    }
+
+    public int getSize(){
+        return thisList.size();
+    }
+}
---
+++ src/test/java/Sudoku/SaveListTest.java
@@ -0,0 +1,57 @@
+package Sudoku;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*; 
+import org.junit.Test;
+import org.junit.Before;
+
+public class SaveListTest {
+    SaveList savelist;
+
+    @Before
+    public void initialize(){
+        savelist = new SaveList();
+    }
+
+    @Test
+    public void addListTest(){
+        ArrayList<Integer> cell00 = new  ArrayList<Integer>(Arrays.asList(0,0,0));
+        ArrayList<Integer> cell88 = new  ArrayList<Integer>(Arrays.asList(8,8,8));
+        assertTrue(savelist.addList(0, 0, cell00));
+        assertTrue(savelist.addList(8, 8, cell88));
+        assertFalse(savelist.addList(-1, -1, cell00));
+        assertFalse(savelist.addList(8, 9, cell88));
+    }
+
+    @Test
+    public void getList(){
+        ArrayList<Integer> cell00 = new  ArrayList<Integer>(Arrays.asList(0,0,0));
+        ArrayList<Integer> cell88 = new  ArrayList<Integer>(Arrays.asList(8,8,8));
+        savelist.addList(0, 0, cell00);
+        savelist.addList(8, 8, cell88); 
+        assertEquals(cell00, savelist.getList(0,0));
+        assertEquals(cell88, savelist.getList(8,8)); 
+        assertEquals(null, savelist.getList(8,9)); 
+        assertEquals(null, savelist.getList(-1,-1)); 
+    }
+
+    @Test
+    public void getIndexFromRowColTest(){ 
+        assertEquals(3, savelist.getIndexFromRowCol(0, 3));
+        assertEquals(8, savelist.getIndexFromRowCol(0, 8));
+        assertEquals(9, savelist.getIndexFromRowCol(1, 0));
+        assertEquals(17, savelist.getIndexFromRowCol(1, 8));
+        assertEquals(18, savelist.getIndexFromRowCol(2, 0));
+        assertEquals(80, savelist.getIndexFromRowCol(8, 8));
+        assertEquals(-1, savelist.getIndexFromRowCol(8, 9));
+    }
+    
+    @Test
+    public void initialSizeTest(){
+        assertEquals(81, savelist.getSize());
+    }
+
+} 
+
